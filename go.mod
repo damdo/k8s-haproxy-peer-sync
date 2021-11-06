ce599bf4f7011995cfb82ff698e0b9c463565676
Damiano Donati <damiano.donati@gmail.com>
2021-11-06 00:58:09 +0000

    syncer

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	k8s.io/api v0.22.3
+	k8s.io/apimachinery v0.22.3
 	k8s.io/client-go v0.22.3
 )
--- main.go
+++ main.go
@@ -2,55 +2,56 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"path/filepath"
 
 	discoveryv1 "k8s.io/api/discovery/v1"
-	"k8s.io/client-go/informers"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	informersdiscoveryv1 "k8s.io/client-go/informers/discovery/v1"
+	"k8s.io/client-go/informers/internalinterfaces"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-	//
-	// Uncomment to load all auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth"
-	//
-	// Or uncomment to load specific auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
 
+var service = flag.String("service", "", "")
+var namespace = flag.String("namespace", "", "")
+var user = flag.String("user", "", "")
+var password = flag.String("password", "", "")
+var dataPlaneAPIAddress = flag.String("data-plane-api-address", "127.0.0.1:5555", "")
+var peerSectionName = flag.String("peer-section-name", "haproxy-peers", "")
+var peersPort = flag.Int("peer-port", 3000, "")
+var hostname string = os.Getenv("HOSTNAME")
+
 func main() {
-	//// in-cluster config
-	//config, err := rest.InClusterConfig()
+
+	// in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//var kubeconfig *string
+	//if home := homedir.HomeDir(); home != "" {
+	//	kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	//} else {
+	//	kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	//}
+	//// use the current context in kubeconfig
+	//config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	//if err != nil {
 	//	panic(err.Error())
 	//}
 
-	var service = flag.String("service", "", "")
-
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-
 	flag.Parse()
 
-	if *service == "" {
+	if *service == "" || *namespace == "" {
 		flag.Usage()
 		os.Exit(1)
-	}
-
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
 	}
 
 	// create the clientset
@@ -59,9 +60,14 @@ func main() {
 		panic(err.Error())
 	}
 
-	factory := informers.NewSharedInformerFactory(clientset, 0)
+	listOptionsFunc := internalinterfaces.TweakListOptionsFunc(func(options *metav1.ListOptions) {
+		*options = metav1.ListOptions{LabelSelector: "kubernetes.io/service-name=" + *service}
+	})
+
+	informer := informersdiscoveryv1.NewFilteredEndpointSliceInformer(clientset, *namespace, 0, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc}, listOptionsFunc)
 	//informer := factory.Core().V1().Pods().Informer()
-	informer := factory.Discovery().V1().EndpointSlices().Informer()
+	//informer := factory.Discovery().V1().EndpointSlice().Informer()
+
 	stopper := make(chan struct{})
 	defer close(stopper)
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
@@ -75,9 +81,35 @@ func main() {
 		},
 		UpdateFunc: func(oldObj, obj interface{}) {
 			mObj := obj.(*discoveryv1.EndpointSlice)
-			log.Printf("ES updated: %s", mObj.GetName())
+
+			var addrs []string
+			for _, v := range mObj.Endpoints {
+				addrs = append(addrs, v.Addresses[0])
+			}
+			log.Printf("ES updated: %s %#v", mObj.GetName(), addrs)
 		},
 	})
 
 	informer.Run(stopper)
+}
+
+func updateHaproxy(addrs []string) {
+	client := &http.Client{}
+
+	for _, addr := range addrs {
+		body := []byte(fmt.Sprintf(`{"name": "%s", "address":"%s", "port":%d}`, hostname, addr, peersPort))
+		req, err := http.NewRequest("POST", "http://"+*dataPlaneAPIAddress+"/services/haproxy/configuration/peer_entries", bytes.NewReader(body))
+		if err != nil {
+			log.Println(err)
+		}
+		req.SetBasicAuth(*user, *password)
+		q := req.URL.Query()
+		q.Add("peer_section", *peerSectionName)
+		req.URL.RawQuery = q.Encode()
+
+		_, err = client.Do(req)
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
