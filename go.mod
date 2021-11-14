bd01e90db8ca819178b70dc468a21995a6706e53
Damiano Donati <damiano.donati@gmail.com>
2021-11-14 15:34:59 +0000

    log -> klog

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	k8s.io/api v0.22.3
 	k8s.io/apimachinery v0.22.3
 	k8s.io/client-go v0.22.3
+	k8s.io/klog/v2 v2.9.0
 )
--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -21,45 +20,44 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
+	klog "k8s.io/klog/v2"
 )
 
-var service = flag.String("service", "", "the Kubernetes Service that looks after the HAProxy pods")
-var namespace = flag.String("namespace", "", "the Kubernetes Namespace where the HAProxy setup lives")
-var user = flag.String("user", "admin", "the username to access the DataPlane API via HTTP Basic Access Authentication")
-var password = flag.String("password", "", "the username to access the DataPlane API via HTTP Basic Access Authentication")
-
-var dataPlaneAPIAddress = flag.String("data-plane-api-address", "127.0.0.1:5555", "(optional) the address (ip:port) where the HAProxy DataPlane API is listening")
-var peerSectionName = flag.String("peer-section-name", "haproxy-peers", "(optional) the name of the peer-section to sync")
-var peersPort = flag.Int("peer-port", 3000, "(optional) the port where HAProxy listens for peer communication")
-var networkInterface = flag.String("network-interface", "eth0", "(optional) the network interface that HAProxy uses for peer communication")
-var startupDelayStr = flag.String("startup-delay", "2s", "(optional) initial delay to wait for HAProxy DataPlane API to be up and running")
-var ownIPAddress string
+var (
+	service             = flag.String("service", "", "the Kubernetes Service that looks after the HAProxy pods")
+	namespace           = flag.String("namespace", "", "the Kubernetes Namespace where the HAProxy setup lives")
+	user                = flag.String("user", "admin", "the username to access the DataPlane API via HTTP Basic Access Authentication")
+	password            = flag.String("password", "", "the username to access the DataPlane API via HTTP Basic Access Authentication")
+	dataPlaneAPIAddress = flag.String("data-plane-api-address", "127.0.0.1:5555", "(optional) the address (ip:port) where the HAProxy DataPlane API is listening")
+	peerSectionName     = flag.String("peer-section-name", "haproxy-peers", "(optional) the name of the peer-section to sync")
+	networkInterface    = flag.String("network-interface", "eth0", "(optional) the network interface that HAProxy uses for peer communication")
+	startupDelayStr     = flag.String("startup-delay", "2s", "(optional) initial delay to wait for HAProxy DataPlane API to be up and running")
+	peersPort           = flag.Int("peer-port", 3000, "(optional) the port where HAProxy listens for peer communication")
+	ownIPAddress        string
+)
 
 func main() {
-	// Enable line numbers in logging
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-
 	flag.Parse()
 
 	if *service == "" || *namespace == "" || *password == "" {
 		flag.Usage()
-		log.Fatalln("error missing non optional flags")
+		klog.Fatalln("error missing non optional flags")
 	}
 	startupDelay, err := time.ParseDuration(*startupDelayStr)
 	if err != nil {
-		log.Fatalln(err)
+		klog.Fatalln(err)
 	}
 
-	log.Println("Starting with config:")
-	log.Printf("service=%#v\n", *service)
-	log.Printf("namespace=%#v\n", *namespace)
-	log.Printf("user=%#v\n", *user)
-	log.Println("password=<REDACTED>")
-	log.Printf("dataPlaneAPIAddress=%#v\n", *dataPlaneAPIAddress)
-	log.Printf("peerSectionName=%#v\n", *peerSectionName)
-	log.Printf("peersPort=%#v\n", *peersPort)
-	log.Printf("networkInterface=%#v\n", *networkInterface)
-	log.Printf("startupDelay=%#v\n", *networkInterface)
+	klog.Info("Starting with config:")
+	klog.Infof("service=%#v\n", *service)
+	klog.Infof("namespace=%#v\n", *namespace)
+	klog.Infof("user=%#v\n", *user)
+	klog.Info("password=<REDACTED>")
+	klog.Infof("dataPlaneAPIAddress=%#v\n", *dataPlaneAPIAddress)
+	klog.Infof("peerSectionName=%#v\n", *peerSectionName)
+	klog.Infof("peersPort=%#v\n", *peersPort)
+	klog.Infof("networkInterface=%#v\n", *networkInterface)
+	klog.Infof("startupDelay=%#v\n", *networkInterface)
 
 	// create a context.Context that is cancelled on an os.Interrupt signal. This allows to prevent the application
 	// from exiting until it receives an interrupt signal. Its 'ctx.Done()' channel is passed to informer.Run, to keep the informer
@@ -67,7 +65,7 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
-	log.Printf("waiting for %s startup delay", *startupDelayStr)
+	klog.Infof("waiting for %s startup delay", *startupDelayStr)
 
 	select {
 	case <-time.After(startupDelay):
@@ -108,16 +106,16 @@ func main() {
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			mObj := obj.(*discoveryv1.EndpointSlice)
-			log.Printf("ES added: %s", mObj.GetName())
+			klog.Infof("ES added: %s", mObj.GetName())
 		},
 		DeleteFunc: func(obj interface{}) {
 			mObj := obj.(*discoveryv1.EndpointSlice)
-			log.Printf("ES deleted: %s", mObj.GetName())
+			klog.Infof("ES deleted: %s", mObj.GetName())
 		},
 		UpdateFunc: func(oldObj, obj interface{}) {
 			mOldObj := oldObj.(*discoveryv1.EndpointSlice)
 			mObj := obj.(*discoveryv1.EndpointSlice)
-			log.Printf("ES updated: '%s'\n", mObj.GetName())
+			klog.Infof("ES updated: '%s'\n", mObj.GetName())
 
 			var oldPeers []Peer
 			for _, v := range mOldObj.Endpoints {
@@ -134,7 +132,7 @@ func main() {
 			}
 
 			toRemove := difference(oldPeers, desiredPeers)
-			log.Printf("ES: '%s', desired: %#v, toRemove: %#v\n", mObj.GetName(), desiredPeers, toRemove)
+			klog.Infof("ES: '%s', desired: %#v, toRemove: %#v\n", mObj.GetName(), desiredPeers, toRemove)
 
 			updatePeers(ctx, desiredPeers, toRemove)
 		},
@@ -201,8 +199,8 @@ func updatePeers(ctx context.Context, desired []Peer, deletions []Peer) {
 	} else {
 		panic(err)
 	}
-	log.Println("transaction: starting transaction against HAProxy DataPlane API")
-	log.Printf("transaction: CREATION, version=%d, transaction_id='%s', status_code=%d\n", version, transactionID, resp.StatusCode)
+	klog.Info("transaction: starting transaction against HAProxy DataPlane API")
+	klog.Infof("transaction: CREATION, version=%d, transaction_id='%s', status_code=%d\n", version, transactionID, resp.StatusCode)
 
 	// idempotently add a new peer_section to the HAProxy config
 	// https://www.haproxy.com/documentation/dataplaneapi/community/#post-/services/haproxy/configuration/peer_section
@@ -221,7 +219,7 @@ func updatePeers(ctx context.Context, desired []Peer, deletions []Peer) {
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("peer_section: '%s' CREATION, status_code=%d\n", *peerSectionName, resp.StatusCode)
+	klog.Infof("peer_section: '%s' CREATION, status_code=%d\n", *peerSectionName, resp.StatusCode)
 
 	// idempotently add the desired `peer_entry`s to the previously created peer_section in the HAProxy config
 	for _, p := range desired {
@@ -248,7 +246,7 @@ func updatePeers(ctx context.Context, desired []Peer, deletions []Peer) {
 		if err != nil {
 			panic(err)
 		}
-		log.Printf("peer_entries: 'peer %s %s:%d' CREATION, status_code=%d\n", p.hostname, p.addresses[0], *peersPort, resp.StatusCode)
+		klog.Infof("peer_entries: 'peer %s %s:%d' CREATION, status_code=%d\n", p.hostname, p.addresses[0], *peersPort, resp.StatusCode)
 	}
 
 	// idempotently delete the unneded `peer_entry`s from the previously created peer_section in the HAProxy config
@@ -276,10 +274,10 @@ func updatePeers(ctx context.Context, desired []Peer, deletions []Peer) {
 		if err != nil {
 			panic(err)
 		}
-		log.Printf("peer_entries: 'peer %s %s:%d' DELETION, status_code=%d\n", p.hostname, p.addresses[0], *peersPort, resp.StatusCode)
+		klog.Infof("peer_entries: 'peer %s %s:%d' DELETION, status_code=%d\n", p.hostname, p.addresses[0], *peersPort, resp.StatusCode)
 	}
 
-	//commit the previously started transaction against the HAProxy DataPlane API
+	// commit the previously started transaction against the HAProxy DataPlane API
 	// https://www.haproxy.com/documentation/dataplaneapi/community/#put-/services/haproxy/transactions/-id-
 	req, err = http.NewRequestWithContext(ctx, "PUT", fmt.Sprintf("http://"+*dataPlaneAPIAddress+"/v2/services/haproxy/transactions/%s", transactionID), nil)
 	if err != nil {
@@ -300,5 +298,5 @@ func updatePeers(ctx context.Context, desired []Peer, deletions []Peer) {
 	if err := json.Unmarshal(body, &resultMap); err != nil {
 		panic(err)
 	}
-	log.Printf("transaction: COMMIT transaction_id='%s', status_code=%d, result=%#v\n", transactionID, resp.StatusCode, resultMap)
+	klog.Infof("transaction: COMMIT transaction_id='%s', status_code=%d, result=%#v\n", transactionID, resp.StatusCode, resultMap)
 }
